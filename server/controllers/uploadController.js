62d93a6bd1b27a68253882685ac94f7339f7a3cc
Henrik Kurkela <henrik.kurkela@gmail.com>
2021-02-17 16:13:09 +0200

    Work on Documentation

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -1,9 +1,8 @@
 const uploadRouter = require('express').Router()
 const fs = require('fs')
-const path = require('path')
 
 const multer = require('multer')
-const upload = multer({ dest: path.join(__dirname, '../../public/assets/img') })
+const upload = multer({ dest: 'public/assets/img/' })
 
 const auth = require('../middlewares/authMiddleware')
 
@@ -16,7 +15,6 @@ uploadRouter.post('/', [upload.single('picture'), auth], (request, response) => 
         fs.unlinkSync(request.file.path)
         response.status(401).send('Unauthorized.')
     } else {
-        console.log(request.file.path)
         response.status(201).json({ filename: `/assets/img/${request.file.filename}` })
     }
 })
